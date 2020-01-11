b2e7ec541ec09bec94db73abfabb7a37be3454f8
Janus <janus@insignificancegalore.net>
2020-01-11 22:47:16 +0100

    Now with death

--- main.ts
+++ main.ts
@@ -1,42 +1,42 @@
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-        . . . . . . . . . . . . . . . .
-    `
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
     //% blockIdentity=images._tile
     export const tile1 = img`
-        . . . . 3 3 3 3 3 3 3 3 . . . .
-        . 3 3 3 3 . . . . . . 3 3 . . .
-        3 3 . . . . . . . . . . 3 3 . .
-        3 3 . . . . . . . . . . . . 3 3
-        . 3 . . . . . . . . . . . . . 3
-        . 3 . . . . . . . . . . . . . 3
-        . 3 . . . . . . . . . . . . . 3
-        . 3 . . . . . . . . . . . . . 3
-        3 . . . . . . . . . . . . . . 3
-        3 . 3 3 3 3 . . . . . . . . . 3
-        3 . 3 3 3 3 . . . . . . . . . 3
-        3 3 3 3 3 3 . . . . . . . . . 3
-        . . 3 3 . . . . . . . . . . . 3
-        . 3 . 3 3 3 3 . . . . . . . . 3
-        . . . . . . . 3 3 . . . . . . 3
-        . . . . . . . . 3 3 3 3 3 3 3 3
-    `
+. . . . 3 3 3 3 3 3 3 3 . . . . 
+. 3 3 3 3 . . . . . . 3 3 . . . 
+3 3 . . . . . . . . . . 3 3 . . 
+3 3 . . . . . . . . . . . . 3 3 
+. 3 . . . . . . . . . . . . . 3 
+. 3 . . . . . . . . . . . . . 3 
+. 3 . . . . . . . . . . . . . 3 
+. 3 . . . . . . . . . . . . . 3 
+3 . . . . . . . . . . . . . . 3 
+3 . 3 3 3 3 . . . . . . . . . 3 
+3 . 3 3 3 3 . . . . . . . . . 3 
+3 3 3 3 3 3 . . . . . . . . . 3 
+. . 3 3 . . . . . . . . . . . 3 
+. 3 . 3 3 3 3 . . . . . . . . 3 
+. . . . . . . 3 3 . . . . . . 3 
+. . . . . . . . 3 3 3 3 3 3 3 3 
+`
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     if (Math.abs(sprite.x - otherSprite.x) > Math.abs(sprite.y - otherSprite.y)) {
@@ -45,43 +45,66 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
         sprite.vy = 0 - sprite.vy
     }
     otherSprite.destroy(effects.disintegrate, 200)
+    info.changeScoreBy(1)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (on_board) {
+        ball.follow(board, 0)
+        ball.y += -8
+        ball.setVelocity(50, -50)
+        on_board = 0
+    }
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
     ball.vy = 0 - ball.vy
 })
+let on_board = 0
 let mySprite: Sprite = null
+let board: Sprite = null
 let ball: Sprite = null
+info.setScore(0)
+info.setLife(3)
+let block = img`
+. 8 8 8 8 8 8 . 
+8 8 1 1 1 1 8 8 
+8 1 1 1 1 1 1 8 
+8 1 1 1 1 1 1 8 
+8 1 1 1 1 1 1 8 
+8 1 1 1 1 1 1 8 
+8 8 1 1 1 1 8 8 
+. 8 8 8 8 8 8 . 
+`
 ball = sprites.create(img`
-    . 3 3 3 .
-    3 3 3 3 3
-    3 3 3 3 3
-    3 3 3 3 3
-    . 3 3 3 .
+. 3 3 3 . 
+3 3 3 3 3 
+3 3 3 3 3 
+3 3 3 3 3 
+. 3 3 3 . 
 `, SpriteKind.Projectile)
-let board = sprites.create(img`
-    . 8 8 8 8 8 8 8 8 8 8 .
-    8 8 8 8 8 8 8 8 8 8 8 8
+board = sprites.create(img`
+. 8 8 8 8 8 8 8 8 8 8 . 
+8 8 8 8 8 8 8 8 8 8 8 8 
 `, SpriteKind.Player)
 ball.setPosition(0, 78)
 board.setPosition(0, 110)
-ball.setVelocity(50, 50)
 ball.setFlag(SpriteFlag.StayInScreen, true)
 ball.setFlag(SpriteFlag.BounceOnWall, true)
 board.setFlag(SpriteFlag.StayInScreen, true)
 controller.moveSprite(board, 100, 0)
-let block = img`
-    . 8 8 8 8 8 8 .
-    8 8 1 1 1 1 8 8
-    8 1 1 1 1 1 1 8
-    8 1 1 1 1 1 1 8
-    8 1 1 1 1 1 1 8
-    8 1 1 1 1 1 1 8
-    8 8 1 1 1 1 8 8
-    . 8 8 8 8 8 8 .
-`
 for (let indexy = 0; indexy <= 5; indexy++) {
-    for (let indexx = 0; indexx <= 12; indexx++) {
+    for (let indexx = 0; indexx <= 14; indexx++) {
         mySprite = sprites.create(block, SpriteKind.Enemy)
-        mySprite.setPosition(8 + 12 * indexx, 20 + 12 * indexy)
+        mySprite.setPosition(10 + 10 * indexx, 20 + 10 * indexy)
     }
 }
+on_board = 1
+ball.follow(board, 250)
+game.onUpdate(function () {
+    if (ball.y > 112) {
+        on_board = 1
+        ball.y += -8
+        info.changeLifeBy(-1)
+        ball.follow(board, 250)
+        scene.cameraShake(4, 200)
+    }
+})
